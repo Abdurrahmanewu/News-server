20cd126163b83e2f9da6d57766c3e46a45711cc1
Abdurrahmanewu <abdurrahman.ewu.bd@gmail.com>
2023-07-07 22:19:53 +0600

    some changes

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const port = process.env.PORT || 5001;
 
 app.use(cors());
+
 const catagories = require("./Data/catagory.json");
+// const news = require("./Data/news.json");
 
 app.get("/", (req, res) => {
   res.send("news API is running after change");
@@ -12,6 +14,7 @@ app.get("/", (req, res) => {
 app.get("/news-catagories", (req, res) => {
   res.send(catagories);
 });
+// app.get("/news/:id", (req, res) => {});
 
 app.listen(port, () => {
   console.log("server on port ", port);
