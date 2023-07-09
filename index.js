fd24b8317b42c9f7620a8de2706cce67045de325
Abdurrahmanewu <abdurrahman.ewu.bd@gmail.com>
2023-07-09 08:24:31 +0600

    next

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const catagories = require("./Data/catagory.json");
-// const news = require("./Data/news.json");
+const catagories = require("./Data/category.json");
+const news = require("./Data/news.json");
 
 app.get("/", (req, res) => {
-  res.send("news API is running after change");
+  res.send("news API is running");
 });
 app.get("/news-catagories", (req, res) => {
   res.send(catagories);
 });
-// app.get("/news/:id", (req, res) => {});
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
+});
 
 app.listen(port, () => {
   console.log("server on port ", port);
